ef7a8fd0ee315f124418c2c1aea401488d6dff37
Riya Fartyal <riyaf3105@gmail.com>
2024-05-13 17:25:36 +0900

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,16 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :mentors do
+    resources :bookings, only: :create
+  end
+
+  resources :bookings, only: [:update, :index]
+
+  namespace :mentor do
+    resources :bookings, only: :index
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
