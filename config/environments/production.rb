62de6094aea8f94b3861d24e31745151fba5eeef
Justin Garcia <justin.garcia@gmail.com>
2024-05-13 15:35:21 +0900

    save

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.default_url_options = { host: 'https://mentors-io-64588d5d8687.herokuapp.com', port: 8080 }
 end
