5521710b9d3478ab98f78232037e370f29efeff3
Shuxing Fang <fangshuxing0613@gmail.com>
2024-05-14 16:08:27 +0900

    booking_page added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,9 +9,13 @@ class BookingsController < ApplicationController
     end
   end
 
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
   private
 
   def strong_params
-    params.require(:booking).permit(:start_time).merge(user: current_user, mentor: @mentor)
+    params.require(:booking).permit(:start_time).merge(user: current_user, mentor: @mentor, status: "pending")
   end
 end
