64efa9c916e961b32885a7282ac6448d0fbde69f
Justin Garcia <justin.garcia@gmail.com>
2024-05-14 17:10:55 +0900

    taglines and bios (#15)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_13_083511) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_14_075007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,7 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_083511) do
     t.datetime "start_time"
     t.bigint "user_id", null: false
     t.bigint "mentor_id", null: false
-    t.string "status"
+    t.string "status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["mentor_id"], name: "index_bookings_on_mentor_id"
@@ -59,6 +59,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_083511) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "tagline"
+    t.string "bio"
     t.index ["user_id"], name: "index_mentors_on_user_id"
   end
 
