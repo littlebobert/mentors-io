4b2d2f78c82620e9e24ed3073581de5c2912308c
Justin Garcia <justin.garcia@gmail.com>
2024-05-16 15:00:15 +0900

    misc stuff (#45)

--- app/controllers/mentor/bookings_controller.rb
+++ app/controllers/mentor/bookings_controller.rb
@@ -3,4 +3,14 @@ class Mentor::BookingsController < ApplicationController
     @mentor = Mentor.where(user: current_user).first
     @bookings = Booking.where(mentor: @mentor)
   end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(status: params["status"])
+    if @booking.save
+      redirect_to mentor_mentor_bookings_path(:status => "pending")
+    else
+      render "mentor/bookings/index", status: :unprocessable_entity
+    end
+  end
 end
