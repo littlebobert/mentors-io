941e2c191571bfddbda9a27d3f0535ce3539661c
Riya Fartyal <riyaf3105@gmail.com>
2024-05-16 12:44:08 +0900

    search bar works

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,6 +1,12 @@
 class MentorsController < ApplicationController
   def index
     @mentors = Mentor.all
+    if params[:query].present?
+      @mentors = Mentor.joins(:user)
+        .where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @mentors = Mentor.all
+    end
   end
 
   def show
