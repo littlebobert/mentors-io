7df999181061919a4caa986d94c5292c6ce5966e
ShuxingFang <151457729+ShuxingFang@users.noreply.github.com>
2024-05-16 12:50:21 +0900

    now a user can become a mentor (#42)

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,4 +1,5 @@
 class MentorsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @mentors = Mentor.all
     if params[:query].present?
@@ -12,5 +13,26 @@ class MentorsController < ApplicationController
   def show
     @mentor = Mentor.find(params[:id])
     @booking = Booking.new
+  end
+
+  def new
+    @mentor = Mentor.new
+  end
+
+  def create
+    @mentor = Mentor.new(mentor_params)
+    @mentor.user = current_user
+
+    if @mentor.save
+      redirect_to @mentor, notice: "mentor was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def mentor_params
+    params.require(:mentor).permit(:specialty, :photo, :price, :tagline, :bio)
   end
 end
